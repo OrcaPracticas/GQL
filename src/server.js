3e208a44a128f8e1792d82804050fd4a4228d9b6
jmendez <jorge.mendez.ortega@gmail.com>
2018-08-29 21:02:00 -0500

    REFACTOR: Integracion de apollo
    Para realizar la integracion con apollo se realizaron modificaciones en 
    el server 
    
    issue #6

--- src/server.js
+++ src/server.js
@@ -1,38 +1,12 @@
 import Express from "express";
-import BodyParser from "body-parser";
-import { graphqlExpress, graphiqlExpress } from "graphql-server-express";
 import Schema from "./schemas";
 import "./DB/setup";
 
 const SERVER = Express();
 const PORT = 8080;
 
-/**
- * Ruta que nos permite acceder al servidor de GQL.
- * en este caso se utiliza el MiddelWare de express.
- */
-SERVER.use(
-    "/gql",
-    BodyParser.json(),
-    graphqlExpress({
-        schema: Schema,
-        formatError: error => ({
-            errorCode: "A55",
-            name: error.name,
-            mensaje: error.message,
-        }),
-    }),
-);
-
-/**
- * Ruta para acceder a la interfaz grafica para GQL.
- */
-SERVER.use(
-    "/client",
-    graphiqlExpress({
-        endpointURL: "/gql",
-    }),
-);
+// ruta por defecto graphql
+Schema.applyMiddleware({ app: SERVER });
 
 /**
  * iniciando el servidor
